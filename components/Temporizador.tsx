4d401c27843ae487852b2c22ce7ae99a19501480
sirAndre1337 <andrelluis17@hotmail.com>
2022-08-19 17:44:08 -0300

    Componente Index, Solucao atributo Key temporizador

--- components/Temporizador.tsx
+++ components/Temporizador.tsx
@@ -3,6 +3,7 @@ import styles from '../styles/Temporizador.module.css';
 
 
 interface TemporizadorProps {
+    key: any
     duracao: number
     tempoEsgotado: () => void
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,16 @@
+import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react'
 import Questionario from '../components/Questionario'
 import QuestaoModel from '../model/questao'
-import RespostaModel from '../model/resposta'
-
-const questaoMock = new QuestaoModel(1, 'Melhor cor?', [
-  RespostaModel.errada('Verde'),
-  RespostaModel.errada('Vermelha'),
-  RespostaModel.errada('Azul'),
-  RespostaModel.certa('Preta'),
-])
 
 const BASE_URL = 'http://localhost:3000/api';
 
-
 export default function Home() {
   const [idsDasQuestoes, setIdsDasQuestoes] = useState<number[]>([])
-  const [questao, setQuestao] = useState<QuestaoModel>(questaoMock);
+  const [questao, setQuestao] = useState<QuestaoModel>();
+  const [respostasCertas, setRespostasCertas] = useState<number>(0);
+
+  const router = useRouter()
 
   async function carregarIDSQuestoes() {
     const resp = await fetch(`${BASE_URL}/questionario`)
@@ -50,18 +45,40 @@ export default function Home() {
     }
   }
 
-  function questaoRespondida(){
+  function questaoRespondida(questaoRespondida: QuestaoModel){
+      setQuestao(questaoRespondida)
+      const acertou = questaoRespondida.acertou;
+      setRespostasCertas(respostasCertas + (acertou ? 1 : 0))
+  }
 
+  function idProximaPergunta() {
+    const proximoIndice = idsDasQuestoes.indexOf(questao?.id) + 1
+    return idsDasQuestoes[proximoIndice]
   }
 
   function irParaProximoPasso() {
+    const proximoId = idProximaPergunta()
+    proximoId ? irParaProximaQuestao(proximoId) : finalizar()
+  }
 
+  function irParaProximaQuestao(proximoId: number) {
+      carregarQuestao(proximoId)
+  }
+
+  function finalizar() {
+      router.push({
+        pathname: '/resultado',
+        query: {
+          total: idsDasQuestoes.length,
+          certas: respostasCertas
+        }
+      })
   }
 
   return (
       <Questionario 
       questao={questao}
-      ultima={false}
+      ultima={idProximaPergunta() ? false : true}
       questaoRespondida={questaoRespondida}
       irParaProximoPasso={irParaProximoPasso}
       />
