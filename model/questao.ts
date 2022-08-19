f8d4ef8d9bf357043639656729ce754f82fa9ae5
sirAndre1337 <andrelluis17@hotmail.com>
2022-08-19 17:11:12 -0300

    Inicio componente Index, transformar JSON para modelo

--- model/questao.ts
+++ model/questao.ts
@@ -50,6 +50,11 @@ export default class QuestaoModel {
         return new QuestaoModel(this.#id, this.#enunciado, respostasEmbaralhas, this.#acertou);
     }
 
+    static criarUsandoObjeto(obj: QuestaoModel): QuestaoModel {
+        const respostas = obj.respostas.map(resp => RespostaModel.criarUsandoObjeto(resp))
+        return new QuestaoModel(obj.id, obj.enunciado, respostas, obj.acertou)
+    }
+
     paraObjeto() {
         return {
             id: this.#id,
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import Botao from '../components/Botao'
-import Questao from '../components/Questao'
+import { useEffect, useState } from 'react'
 import Questionario from '../components/Questionario'
 import QuestaoModel from '../model/questao'
 import RespostaModel from '../model/resposta'
@@ -12,10 +10,33 @@ const questaoMock = new QuestaoModel(1, 'Melhor cor?', [
   RespostaModel.certa('Preta'),
 ])
 
+const BASE_URL = 'http://localhost:3000/api';
+
 
 export default function Home() {
+  const [idsDasQuestoes, setIdsDasQuestoes] = useState<number[]>([])
+  const [questao, setQuestao] = useState<QuestaoModel>(questaoMock);
 
-  const [questao, setQuestao] = useState(questaoMock);
+  async function carregarIDSQuestoes() {
+    const resp = await fetch(`${BASE_URL}/questionario`)
+    const idsDasQuestoes = await resp.json()
+    setIdsDasQuestoes(idsDasQuestoes)
+  }
+
+  async function carregarQuestao(idQuestao: number) {
+    const resp = await fetch(`${BASE_URL}/questoes/${idQuestao}`)
+    const obj = await resp.json()
+    const novaQuestao = QuestaoModel.criarUsandoObjeto(obj);
+    setQuestao(novaQuestao);
+  }
+
+  useEffect(() => {
+    carregarIDSQuestoes()
+  },[])
+
+  useEffect(() => {
+    idsDasQuestoes.length > 0 && carregarQuestao(idsDasQuestoes[0])
+  },[idsDasQuestoes])
 
   function respostaFornecida(indice: number) {
     console.log(indice);
@@ -38,19 +59,11 @@ export default function Home() {
   }
 
   return (
-    <div style={{
-      display: 'flex',
-      justifyContent: 'center',
-      flexDirection : 'column',
-      alignItems: 'center',
-      height: '100vh',
-    }}>
       <Questionario 
       questao={questao}
       ultima={false}
       questaoRespondida={questaoRespondida}
       irParaProximoPasso={irParaProximoPasso}
       />
-    </div>
   )
 }
