d04291ceec840643affc36740909052b0c0e9f3e
sirAndre1337 <andrelluis17@hotmail.com>
2022-08-29 15:55:26 -0300

    Ajustando base url

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react'
 import Questionario from '../components/Questionario'
 import QuestaoModel from '../model/questao'
 
-const BASE_URL = 'http://localhost:3000/api';
+const BASE_URL = 'https://quiz-sirandre1337.vercel.app/api';
 
 export default function Home() {
   const [idsDasQuestoes, setIdsDasQuestoes] = useState<number[]>([])
